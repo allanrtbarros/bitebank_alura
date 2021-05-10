55678d07f0d40578c492960bdf387d6099522765
Allan <allan@MacBook-Pro.local>
2021-05-10 16:17:10 +0100

    criação do arquivo Conta, reutilizando codigos

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupança } from "./ContaPoupança.js";
+import {Conta} from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309); 
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 
-const contaPoupança = new ContaPoupança(50, cliente1, 1001);
+const contaPoupança = new Conta(50, cliente1, 1001);
 
 console.log(contaCorrenteRicardo);
 console.log(contaPoupança);
