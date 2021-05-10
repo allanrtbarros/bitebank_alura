a8fdbca22bb7ebe85e321a74a9b2e88321f9fb39
Allan <allan@MacBook-Pro.local>
2021-05-10 17:04:51 +0100

    especificando o comportamento sacar das contas

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(0, cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
 const contaPoupança = new ContaPoupança(50, cliente1, 1001);
+contaPoupança.sacar = 10;
 
-console.log(contaCorrenteRicardo);
 console.log(contaPoupança);
+console.log(contaCorrenteRicardo);
