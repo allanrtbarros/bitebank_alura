e74df50c981035e17cd2baa738d1bef374716ab9
allan barros <artbarros.pt@gmail.com>
2021-08-08 12:52:52 +0100

    resolução dos erros

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -1,6 +1,6 @@
 import { Conta } from "./Conta.js";
 
-export class ContaPoupança extends Conta{
+export class ContaPoupanca extends Conta{
     constructor(saldoInicial, cliente, agencia){
         super(saldoInicial, cliente, agencia);
     }
